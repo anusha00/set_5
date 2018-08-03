d982e705f21851f126af1f33218265eb6c10adaf
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-03 17:05:01 +0530

    Update String_Print.java

--- String_Print.java
+++ String_Print.java
@@ -8,7 +8,7 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner s=new Scanner(System.in);
-		String str=s.nextLine();
+		String str=s.next();
 		int n=s.nextInt();
 		for(int i=0;i<n;i++)
 		System.out.println(str);
