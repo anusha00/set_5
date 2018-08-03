954048e3ed87d7bb43615f1f1984bc5383aa6b2a
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-03 19:00:50 +0530

    Create Check_2Pow.java

---
+++ Check_2Pow.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();int f=0,po=1;
+		if(n%2==0)
+		{
+			int i=2;
+			while(po<=n)
+			{
+				po=po*2;
+				if(po==n)
+				f=1;
+			}
+			if(f==1)
+			System.out.print("yes");
+			else
+			System.out.print("no");
+		}
+		else
+		System.out.print("no");
+	}
+}
