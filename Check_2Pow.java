f27a4765d3ab909f0e5b4f396201fa8f2a3ad2b8
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-03 19:02:43 +0530

    Update Check_2Pow.java

--- Check_2Pow.java
+++ Check_2Pow.java
@@ -9,7 +9,9 @@ class Ideone
 	{
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();int f=0,po=1;
-		if(n%2==0)
+		if(n==1)
+			System.out.print("yes");
+		else if(n%2==0)
 		{
 			int i=2;
 			while(po<=n)
