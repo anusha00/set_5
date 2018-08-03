11b7e855343187310f6f92d87bd2d332b43c696a
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-03 18:41:31 +0530

    Create Array_Avg.java

---
+++ Array_Avg.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=s.nextInt();
+		int sum=0;
+		for(int i=0;i<n;i++)
+		{
+			sum=sum+a[i];
+		}
+		System.out.println(sum/n);
+	}
+}
