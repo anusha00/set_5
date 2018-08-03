1e2f5b3ece9ffa2d09a9e13c579884cb291f8294
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-03 17:03:16 +0530

    Create String_Print.java

---
+++ String_Print.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		int n=s.nextInt();
+		for(int i=0;i<n;i++)
+		System.out.println(str);
+	}
+}
