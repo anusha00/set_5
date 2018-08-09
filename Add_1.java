e535feccf5eba07f09edb1138b3ae8beaf314f78
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-09 11:33:19 +0530

    Update and rename Add_One.java to Add_1.java

--- Add_1.java
+++ Add_1.java
@@ -3,12 +3,15 @@ import java.lang.*;
 import java.io.*;
 
 
-class Ideone
+class Add_1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt();
-		System.out.print(n+1);
+		int m=s.nextInt();
+		if(m<10000000)
+		System.out.print(m+1);
+		else
+			System.out.print("Invalid");
 	}
 }
